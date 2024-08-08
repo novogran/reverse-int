6eff9f7c17efbc62173a33f374e1724fd77ac611
Vitaliy <vi19954@gmail.com>
2024-08-08 18:11:07 +0300

    feat: make a solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    n = Math.abs(n);
+    for(let i = n.toString().length-1; i >= 0 ; --i) {
+        result +=n.toString()[i];
+    }
+    return result;
 }
